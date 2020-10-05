55e23f15828749c1f3b094677c500b57922fe591
Filipe Merker <filipesmerker@gmail.com>
2020-10-05 19:03:10 +0200

    Fix auth issue

--- src/api/index.js
+++ src/api/index.js
@@ -7,9 +7,8 @@ const instance = axios.create({
   baseURL: `${corsPrefix}https://api.meetup.com/`
 })
 
-const getRsvpList = () => instance.get(`Meetup-de-Vue-js-Porto-Alegre/events/241796363/rsvps?key=${key}&photo-host=public&sig_id=234792414&sig=4a6dfd4a3d24d85076603cd2c4327771d4425591`)
+const getRsvpList = () => instance.get(`Meetup-de-Vue-js-Porto-Alegre/events/241796363/rsvps`)
 const getMember = id => instance.get(`2/member/${id}?&key=${key}&sign=true&photo-host=public&hash=${Math.random()}`)
-
 
 export default {
   getRsvpList,
